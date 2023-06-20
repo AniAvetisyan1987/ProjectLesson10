0c052cbca27ca278b0c7f038bae59dbf10875926
Armen <Armen@DESKTOP-IRSKKA4>
2023-06-20 19:49:48 +0400

    Add project files.

---
+++ ProjectLesson10/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProjectLesson10;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double salary = 200000;
+        SalaryClass salary1 = new SalaryClass(ref salary, 0.2f, 0.05f, 0.015f);
+        
+
+        salary1.SalaryCalc();
+        Console.WriteLine(salary);
+
+        Console.ReadKey();
+    }
+}
---
+++ ProjectLesson10/SalaryClass.cs
@@ -0,0 +1,28 @@
+﻿namespace ProjectLesson10
+{
+    internal class SalaryClass
+    {
+        public double salary;
+        public float tax;
+        public float social;
+        public float stamp;
+
+        public SalaryClass(ref double  salary, float tax, float social, float stamp)
+        {
+            this.salary = salary;
+            this.tax = tax;                
+            this.social = social;
+            this.stamp = stamp;
+            SalaryCalc();
+            salary = this.salary;
+
+        }
+
+        public void  SalaryCalc()
+        {
+            double percent = tax + social + stamp;
+            salary = Math.Round(salary * percent);
+
+        }
+    }
+}
